5eef241d1e471059680f9791bf8c8a95373f96e8
Skye Purchase <atp45@cam.ac.uk>
2024-02-08 15:54:46 +0000

    Add chemistry parser to the library exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import parseMathsExpression from './parseMaths';
 import parseBooleanExpression from './parseBool';
+import parseChemistryExpression from './parseChem';
 
 // This is now DEPRECATED and you should use parseMathsExpression instead.
 const parseExpression = parseMathsExpression;
 
-export { parseExpression, parseMathsExpression, parseBooleanExpression };
+export { parseExpression, parseMathsExpression, parseBooleanExpression, parseChemistryExpression };
